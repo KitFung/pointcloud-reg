4b86cacc4c575b95fa1eb1b6b3b98b42513512cf
KitFung <sa9510@gmail.com>
2021-11-28 22:36:36 +0800

    [Kdtree] Building the build tree flow for kdtree

--- src/kdtree/kdtree_basic.rs
+++ src/kdtree/kdtree_basic.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{rc::Rc, usize};
 
 #[repr(C)]
 pub struct Point<T> {
@@ -23,13 +23,90 @@ where
 }
 
 type FieldType = f32;
+type DistanceType = f32;
 type Point3D = Point<FieldType>;
 
-struct TreeNode<const Dim: usize> {
-    // data: Rc<Vec<Point3D>>
+/// The Actual Data is Stored in KdTree.
+/// This only keep the index of the corresponding data
+/// Datas corresponding to one data will be located sequentially in the actual data array
+struct LeafNodeData {
+    /// Start offset of the datas
+    left_offset: usize,
+    /// End offset of the datas
+    right_offset: usize,
 }
 
-type PointPtr<const Dim: usize> = Box<TreeNode<Dim>>;
+struct NonLeafNodeData {
+    /// Dimension used for div
+    divdim: usize,
+    /// Value used for sub div
+    divlow: DistanceType,
+    /// Value used for sub div
+    divhigh: DistanceType,
+}
+
+enum TreeNodeData {
+    // data: Rc<Vec<Point3D>>
+    LeafNodeData(LeafNodeData),
+    NonLeafNodeData(NonLeafNodeData),
+    None,
+}
+
+struct TreeNode {
+    /// The internal data
+    data: TreeNodeData,
+    /// Left Child Node
+    left_child: Option<Box<TreeNode>>,
+    /// Right Child Node
+    right_child: Option<Box<TreeNode>>,
+}
+
+impl Default for TreeNode {
+    fn default() -> Self {
+        Self {
+            data: TreeNodeData::None,
+            left_child: None,
+            right_child: None,
+        }
+    }
+}
+
+impl TreeNode {
+    pub fn new() -> Self {
+        TreeNode::default()
+    }
+
+    pub fn new_leafnode(left_offset: usize, right_offset: usize) -> Self {
+        Self {
+            data: TreeNodeData::LeafNodeData(LeafNodeData {
+                left_offset,
+                right_offset,
+            }),
+            ..Default::default()
+        }
+    }
+
+    pub fn new_non_leafnode(
+        left_child: Box<TreeNode>,
+        right_child: Box<TreeNode>,
+        divdim: usize,
+        divlow: DistanceType,
+        divhigh: DistanceType,
+    ) -> Self {
+        Self {
+            left_child,
+            right_child,
+            data: TreeNodeData::LeafNodeData(NonLeafNodeData {
+                divdim,
+                divlow,
+                divhigh,
+            }),
+            ..Default::default()
+        }
+    }
+}
+
+type PointPtr = Box<TreeNode>;
 
 #[derive(Default, Clone, Copy)]
 struct Bounding {
@@ -37,31 +114,31 @@ struct Bounding {
     high: FieldType,
 }
 
-type BoundingBox<const Dim: usize> = [Bounding; Dim];
+type BoundingBox<const DIM: usize> = [Bounding; DIM];
 
-pub struct KdTree<const Dim: usize> {
+pub struct KdTree<const DIM: usize> {
     dataset: Vec<Point3D>,
     /// Create KdTree Object and Build Kdtree Index is 2 step
-    root_node: Option<PointPtr<Dim>>,
-    root_bbox: BoundingBox<Dim>,
+    root_node: Option<PointPtr>,
+    root_bbox: BoundingBox<DIM>,
+    /// If least or equal to m_leaf_max_size points, store as leafnode
+    m_leaf_max_size: usize,
 }
 
-impl<const Dim: usize> KdTree<Dim> {
-    pub fn new(dataset: Vec<Point3D>) -> KdTree<Dim> {
+impl<const DIM: usize> KdTree<DIM> {
+    pub fn new(dataset: Vec<Point3D>) -> KdTree<DIM> {
         assert!(!dataset.is_empty(), "Not Support empty pointset");
         Self {
             dataset,
             root_node: None,
-            root_bbox: [Bounding::default(); Dim],
+            root_bbox: [Bounding::default(); DIM],
+            m_leaf_max_size: 10,
         }
     }
 
     pub fn build_index(&mut self) -> &Self {
-        if self.root_node.is_some() {
-            // TODO or just error?
-            self.root_node = None;
-        }
-
+        // Ensure old status is invalided
+        self.root_node = None;
         self.compute_bounding_box();
 
         let mut bbox = self.root_bbox;
@@ -71,34 +148,84 @@ impl<const Dim: usize> KdTree<Dim> {
         self
     }
 
-    fn compute_bounding_box(&mut self) {
-        let pts = self.dataset.as_slice();
+    fn compute_bounding_box_for_range(
+        &self,
+        left: usize,
+        right: usize,
+        bbox: &mut BoundingBox<DIM>,
+    ) {
+        let pts = self.dataset[left + 1..right].iter();
 
-        let field = pts[0].as_slice();
-        for i in 0..Dim {
-            self.root_bbox[i].low = field[i];
-            self.root_bbox[i].high = field[i];
+        let field = self.dataset[left].as_slice();
+        for i in 0..DIM {
+            bbox[i].low = field[i];
+            bbox[i].high = field[i];
         }
 
         for pt in pts {
             let field = pt.as_slice();
-            for i in 0..Dim {
-                self.root_bbox[i].low = self.root_bbox[i].low.min(field[i]);
-                self.root_bbox[i].high = self.root_bbox[i].high.max(field[i]);
+            for i in 0..DIM {
+                bbox[i].low = bbox[i].low.min(field[i]);
+                bbox[i].high = bbox[i].high.max(field[i]);
             }
         }
     }
 
+    fn compute_bounding_box(&mut self) {
+        let mut bbox = self.root_bbox;
+        self.compute_bounding_box_for_range(0, self.dataset.len(), &mut bbox);
+        self.root_bbox = bbox;
+    }
+
+    /// The bbox is used for middleSplit Compute
+    /// Pass by reference is because we want to avoid unnecessary creation
+    /// Handle Points index: left_offset <= x < right_offset
     fn divide_tree(
         &mut self,
         left_offset: usize,
         right_offset: usize,
-        bbox: &mut BoundingBox<Dim>,
-    ) -> Box<TreeNode<Dim>> {
-        Box::new(TreeNode::<Dim> {})
+        bbox: &mut BoundingBox<DIM>,
+    ) -> Box<TreeNode> {
+        if left_offset + self.m_leaf_max_size > right_offset {
+            // Compute BoundingBox
+            self.compute_bounding_box_for_range(left_offset, right_offset + 1, bbox);
+
+            // Store as leafnode
+            Box::new(TreeNode::new_leafnode(left_offset, right_offset))
+        } else {
+            // Split the plane
+            let (ind, cutdim, cutval) =
+                self.split_at_middle(left_offset, right_offset - left_offset, &bbox);
+
+            let mut left_bbox = bbox.clone();
+            let left_node = self.divide_tree(left_offset, left_offset + ind, &mut left_bbox);
+
+            let mut right_bbox = bbox.clone();
+            let right_node = self.divide_tree(left_offset + ind, right_offset, &mut right_bbox);
+
+            Box::new(TreeNode::new_non_leafnode(
+                left_node,
+                right_node,
+                cutdim,
+                left_bbox[cutdim].low,
+                right_bbox[cutdim].high,
+            ))
+        }
     }
 
-    fn split_at_middle(&mut self, ind: usize) {}
+    /// Return (split index, cut DIMension, cut value)
+    fn split_at_middle(
+        &mut self,
+        ind: usize,
+        count: usize,
+        &bbox: &BoundingBox<DIM>,
+    ) -> (usize, usize, DistanceType) {
+        let eps = 0.00001 as DistanceType;
+        let iter = self.dataset.iter().skip(ind).take(count);
+        let dim_iter = (0..DIM).map(|i| bbox[i].high - bbox[i].low);
+        let max_span = dim_iter.fold(0.0 as DistanceType, |l, r| l.max(r));
+        panic!("")
+    }
 
     fn split_plane(&mut self, ind: usize, count: usize, curfeat: i32) {}
 
@@ -107,8 +234,8 @@ impl<const Dim: usize> KdTree<Dim> {
     }
 }
 
-impl<const Dim: usize> KdTree<Dim> {
-    pub fn query_with_bounding_box(&self, bbox: &[FieldType; Dim]) -> Vec<Point3D> {
+impl<const DIM: usize> KdTree<DIM> {
+    pub fn query_with_bounding_box(&self, bbox: &[FieldType; DIM]) -> Vec<Point3D> {
         Vec::new()
     }
     pub fn query_k_nearest_neighor(&self, target: &Point3D, k_neighbor: u32) -> Vec<Point3D> {
